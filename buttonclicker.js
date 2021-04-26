afac0571bd012a40d2cbbd88e82d1673c3744742
Nardos Lengo <nardi.8199@gmail.com>
2021-04-25 23:28:00 -0700

    remove concat

--- buttonclicker.js
+++ buttonclicker.js
@@ -1,8 +1,7 @@
 
 var num = 1;
 function displayNumber() {
-    console.log(num);
     num++;    
-    document.getElementById("pop").innerHTML += num + ",";   
+    document.getElementById("pop").innerHTML = num ;   
 }
 displayNumber();
